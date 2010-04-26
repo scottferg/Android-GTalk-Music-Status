d2f2b95202665b0c2dc6026ed05650e2da646a00
Scott Ferguson <scottwferg@gmail.com>
2010-04-26 10:51:24 -0500

    Initial commit

---
+++ src/com/android/gtalkstatus/GTalkStatusActivity.java
@@ -0,0 +1,15 @@
+package com.android.gtalkstatus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GTalkStatusActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
